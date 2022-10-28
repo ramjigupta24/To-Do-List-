ca7ec38c129f5cf55ec674097005d19c39d3feae
Ram Ji Gupta <114168538+ramjigupta24@users.noreply.github.com>
2022-10-28 23:28:27 +0530

    Add files via upload

---
+++ todo list.js
@@ -0,0 +1,40 @@
+let input = document.querySelector('.to-do-list');
+let addBtn = document.querySelector('.add-list');
+let result = document.querySelector('.result');
+
+input.addEventListener('keyup', () => {
+    if (input.value.trim() != 0) {
+        addBtn.classList.add('active')
+    } else {
+        addBtn.classList.remove('active')
+    }
+})
+
+addBtn.addEventListener('click', () => {
+    if (input.value.trim() != 0) {
+        let newItem = document.createElement('div');
+        newItem.classList.add('item');
+        newItem.innerHTML = `
+        <p>${input.value}</p>
+        <div class="item-btn">
+        <i class="fa-sharp fa-solid fa-circle-check"></i>
+        <i class="fa-solid fa-trash"></i></div>`
+        result.appendChild(newItem);
+        input.value = '';
+
+    }
+    else {
+        alert("Please enter a task");
+    }
+})
+result.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-trash')) {
+        e.target.parentElement.parentElement.remove();
+    }
+})
+
+result.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-circle-check')) {
+        e.target.parentElement.parentElement.classList.toggle('completed');
+    }
+})
